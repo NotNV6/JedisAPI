28d122729da515538520a107ea54f9d2b04f8876
NotNV6 <41305340+NotNV6@users.noreply.github.com>
2019-10-11 01:57:37 +0200

    Update RedisHandler.java

--- src/main/java/rip/skyland/redis/redis/RedisHandler.java
+++ src/main/java/rip/skyland/redis/redis/RedisHandler.java
@@ -70,7 +70,7 @@ private String host;
                             exception.printStackTrace();
                         }
                     }
-                }, "core");
+                }, "example");
             });
         }).start();
 
