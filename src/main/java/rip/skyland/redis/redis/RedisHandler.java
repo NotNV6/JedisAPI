1e91fea59c6e176a6bccd437b4153eb89c6a0694
Alexander P <pcrunn@pm.me>
2019-10-10 19:09:29 +0300

    clean up connect method

--- src/main/java/rip/skyland/redis/redis/RedisHandler.java
+++ src/main/java/rip/skyland/redis/redis/RedisHandler.java
@@ -1,11 +1,12 @@
 package rip.skyland.redis.redis;
 
 import com.google.gson.Gson;
-import lombok.Getter;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPubSub;
 import rip.skyland.redis.redis.packet.Packet;
+
+import javax.swing.*;
 
 public class RedisHandler {
 
@@ -17,7 +18,6 @@ public class RedisHandler {
 
     private String channel;
 
-    @Getter
     private Gson gson;
 
     /**
@@ -36,25 +36,50 @@ public class RedisHandler {
         this.channel = "example";
     }
 
-    public void load() {
+    /**
+     * Attempts to make a connection to the
+     * redis database with the specified credentials and
+     * starts a thread for receiving messages
+     */
+    public void connect() {
         this.jedisPool = new JedisPool(host, port);
-        jedisPool.getResource().auth(password.isEmpty() ? null : password);
+        this.jedisPool.getResource().auth(password.isEmpty() ? null : password);
 
-        new Thread(() ->
-            runCommand(redis -> {
-                redis.auth(password.isEmpty() ? null : password);
+        // Setup thread
+
+        new Thread(() -> {
+
+            this.runCommand(redis -> {
+
+                // authenticate with the specified credentials
+                redis.auth(this.password.isEmpty() ? null : this.password);
+
                 redis.subscribe(new JedisPubSub() {
+
                     @Override
                     public void onMessage(String channel, String message) {
+
                         try {
+
+                            // Create the packet
                             int index = message.indexOf("||");
-                            ((Packet) new Gson().fromJson(message.substring(index + 2), Class.forName(message.substring(0, index)))).onReceive();
-                        } catch (ClassNotFoundException | NoClassDefFoundError exception) {
+                            Object jsonObject = gson.fromJson(message.substring(index + 2), Class.forName(message.substring(0, index)));
+                            Packet packet = (Packet) jsonObject;
+
+                            packet.onReceive();
+
+                        } catch (Exception exception) {
                             exception.printStackTrace();
                         }
+
                     }
-                }, "core");
-            })).start();
+
+                });
+
+            });
+
+        }).start();
+
     }
 
     /**
